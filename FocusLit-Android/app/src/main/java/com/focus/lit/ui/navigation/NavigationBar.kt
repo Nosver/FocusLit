0862955f93c81db653367e85e23ce08beca06d4b
Doğukan <86402638+Dogukan0101@users.noreply.github.com>
2025-03-25 23:55:55 +0300

    navigation bar directory is changed.

--- FocusLit-Android/app/src/main/java/com/focus/lit/ui/navigation/NavigationBar.kt
+++ FocusLit-Android/app/src/main/java/com/focus/lit/ui/navigation/NavigationBar.kt
@@ -1,33 +1,22 @@
-package com.focus.lit.ui.view
+package com.focus.lit.ui.navigation
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.AddCircle
-import androidx.compose.material.icons.outlined.Favorite
-import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
-import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.focus.lit.ui.view.Screen
 
 @Composable
 fun NavigationBarSample(navController: NavController) {

266b6538ecf3ce6b11250119abd5952814f95a87
Kml159 <kml.frk.598@gmail.com>
2025-04-05 14:56:47 +0300

    POST /createSession endpoint implemented.

--- FocusLit-Server/src/main/java/com/focus/lit/mapper/SessionMapper.java
+++ FocusLit-Server/src/main/java/com/focus/lit/mapper/SessionMapper.java
@@ -7,7 +7,6 @@ import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface SessionMapper {
-
     SessionDto sessionToSessionDto(Session session);
     Session sessionDtoToSession(SessionDto sessionDto);
 }

d535cf357141aea92338a6dcc0b8bc33a728f77e
MasisOznigolyan <oznigolyan3@gmail.com>
2025-04-06 22:49:58 +0300

    Update DatabaseSourceConfig.java

--- FocusLit-Server/src/main/java/com/focus/lit/configuration/DatabaseSourceConfig.java
+++ FocusLit-Server/src/main/java/com/focus/lit/configuration/DatabaseSourceConfig.java
@@ -1,6 +1,5 @@
-package com.focus.lit.controller.configuration;
+package com.focus.lit.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

f732c37d245620c32417d88b52e98e8ca7ca3305
Kml159 <kml.frk.598@gmail.com>
2025-04-20 13:11:57 +0300

    /userAnalytics/update in progress

--- FocusLit-Server/src/main/java/com/focus/lit/controller/UserAnalyticsController.java
+++ FocusLit-Server/src/main/java/com/focus/lit/controller/UserAnalyticsController.java
@@ -2,21 +2,14 @@ package com.focus.lit.controller;
 
 
 import com.focus.lit.dto.UserAnalyticsDto;
-import com.focus.lit.dto.UserDto;
 import com.focus.lit.mapper.UserAnalyticsMapper;
-import com.focus.lit.model.User;
 import com.focus.lit.model.UserAnalytics;
 import com.focus.lit.service.UserAnalyticsService;
-import com.focus.lit.service.impl.UserAnalyticsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -39,8 +32,9 @@ public class UserAnalyticsController {
         UserAnalyticsDto userAnalyticsDto = userAnalyticsMapper.userAnalyticsToUserAnalyticsDto(userAnalytics.get());
         return new ResponseEntity<>(userAnalyticsDto, HttpStatus.OK);
     }
+
     @GetMapping("/getUserAchievements")
-    public ResponseEntity<?> getUserAchievemnts(@RequestParam int userAnalyticsId) throws Exception {
+    public ResponseEntity<?> getUserAchievements(@RequestParam int userAnalyticsId) throws Exception {
         try{
             return new ResponseEntity<>(userAnalyticsService.getUserAchievements(userAnalyticsId), HttpStatus.OK);
         }
@@ -50,4 +44,14 @@ public class UserAnalyticsController {
         }
     }
 
+    @PostMapping("/update")
+    public ResponseEntity<UserAnalyticsDto> update(@RequestBody UserAnalyticsDto userAnalyticsDto) {
+        try{
+            return new ResponseEntity<>(userAnalyticsService.update(userAnalyticsDto), HttpStatus.OK);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }

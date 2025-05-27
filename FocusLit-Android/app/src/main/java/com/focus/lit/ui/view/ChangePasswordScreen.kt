1cffda6648bddaa547558a91a39454f9905555eb
Doğukan <86402638+Dogukan0101@users.noreply.github.com>
2025-05-27 18:04:48 +0300

    Loading bar is added to change password screen

--- FocusLit-Android/app/src/main/java/com/focus/lit/ui/view/ChangePasswordScreen.kt
+++ FocusLit-Android/app/src/main/java/com/focus/lit/ui/view/ChangePasswordScreen.kt
@@ -1,7 +1,9 @@
 package com.focus.lit.ui.view
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +15,7 @@ import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -28,11 +31,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.focus.lit.ui.viewmodel.ChangePasswordViewModel
 
@@ -102,6 +107,22 @@ fun ChangePasswordScreen(
                 enabled = (viewModel.currentPassword.isNotEmpty() && viewModel.newPassword.isNotEmpty() && viewModel.confirmPassword.isNotEmpty())
             ) {
                 Text("Change Password")
+            }
+        }
+    }
+    if (viewModel.loading) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.4f)),
+            contentAlignment = Alignment.Center  // Correct placement
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CircularProgressIndicator(color = Color.White)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text("Loading...", color = Color.White, fontSize = 16.sp)
             }
         }
     }
